f34af3c26443f8da4e446a62abb25b5b3bb881c2
darkydtm <boat-appear-portal@duck.com>
2026-08-24 19:52:55 +0500

    test: define mesh relay behavior

---
+++ core/common/src/test/kotlin/com/netless/network/MeshRelayTest.kt
@@ -0,0 +1,32 @@
+package com.netless.network
+
+import com.netless.common.NodeId
+import com.netless.common.PacketId
+import com.netless.common.TransferPolicy
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MeshRelayTest {
+	private val node = MeshRelayNode(NodeId("relay"))
+
+	@Test
+	fun relaysPacketOnceAndSuppressesDuplicate() {
+		val packet = RelayPacket(PacketId("packet"), NodeId("destination"), byteArrayOf(1), ttl = 3, hops = 0)
+
+		assertEquals(RelayDecision.Relayed, node.handle(packet, TransferPolicy()))
+		assertEquals(RelayDecision.Duplicate, node.handle(packet, TransferPolicy()))
+		assertEquals(packet, node.relayed.single())
+	}
+
+	@Test
+	fun rejectsExpiredAndOverHopPackets() {
+		assertEquals(
+			RelayDecision.Expired,
+			node.handle(RelayPacket(PacketId("expired"), NodeId("destination"), byteArrayOf(1), 0, 0), TransferPolicy()),
+		)
+		assertEquals(
+			RelayDecision.MaxHops,
+			node.handle(RelayPacket(PacketId("hops"), NodeId("destination"), byteArrayOf(1), 3, 2), TransferPolicy(maxHops = 2)),
+		)
+	}
+}
