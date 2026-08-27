ab7e7635cd4ac6452e02403f9a8d29890e384fe4
darkydtm <boat-appear-portal@duck.com>
2026-08-27 17:30:54 +0500

    add Gradle wrapper for Android CI

---
+++ gradlew.bat
@@ -0,0 +1,5 @@
+@echo off
+set DIR=%~dp0
+if defined JAVA_HOME set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+if not defined JAVA_HOME set JAVA_EXE=java.exe
+"%JAVA_EXE%" -classpath "%DIR%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
